19ecfc71bf6fb341d8b12c7f60b1506c08e4dccf
petra98 <43559960+petra98@users.noreply.github.com>
2018-09-25 09:34:16 +0700

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,44 @@
+package Basic;
+    public class basic{
+        public static void main(String[] args) {
+            System.out.println("Hy, My name is petra");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
